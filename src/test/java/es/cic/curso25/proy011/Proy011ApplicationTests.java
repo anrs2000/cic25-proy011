b44c75c2f74d52e87a20032f9313db251eaca236
angela <arsoto@formacion.cic.es>
2025-08-05 12:10:25 +0200

    Eliminados archivos de test innecesarios

--- src/test/java/es/cic/curso25/proy011/Proy011ApplicationTests.java
+++ src/test/java/es/cic/curso25/proy011/Proy011ApplicationTests.java
@@ -2,13 +2,11 @@ package es.cic.curso25.proy011;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
-@ActiveProfiles("testcontainer")
+@ActiveProfiles("test")
 class Proy011ApplicationTests {
 
 	@Test
