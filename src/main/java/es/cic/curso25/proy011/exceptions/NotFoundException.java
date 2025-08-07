cf8193bd7f32eb23d1af552a89be54e54167a907
angela <arsoto@formacion.cic.es>
2025-08-07 11:24:52 +0200

    cambios menores

--- src/main/java/es/cic/curso25/proy011/exceptions/NotFoundException.java
+++ src/main/java/es/cic/curso25/proy011/exceptions/NotFoundException.java
@@ -1,16 +1,9 @@
 package es.cic.curso25.proy011.exceptions;
 
 public class NotFoundException extends RuntimeException {
-    public NotFoundException() {
-        super();
-    }
 
     public NotFoundException(String mensaje) {
         super(mensaje);
-    }
-
-    public NotFoundException(Throwable error) {
-        super(error);
     }
 
     public NotFoundException(String mensaje, Throwable error) {
